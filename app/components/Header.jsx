6ccf66b0aefe6fc85fe5acf8d4c8b6ae53d73906
more0210 <michael.ore.dev@gmail.com>
2025-01-09 21:24:48 -0500

    header component

---
+++ app/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <container className='flex justify-center align-between mt-4'>
+        <div className="flex p-8">Golf</div>
+        <div className="flex p-8">HOA</div>
+    </container>
+  )
+}
