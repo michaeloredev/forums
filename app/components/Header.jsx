7d0c9ee1931cc605cabf6b00fcbc4f09edaedfb8
more0210 <michael.ore.dev@gmail.com>
2025-01-09 21:33:12 -0500

    basic route setup

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <container className='flex justify-center align-between mt-4'>
-        <div className="flex p-8">Golf</div>
-        <div className="flex p-8">HOA</div>
+        <div className="flex p-8">
+            <Link href='/'>Welcome</Link>
+        </div>
+        <div className="flex p-8">
+            <Link href='/pages/golf'>Golf</Link>
+        </div>
+        <div className="flex p-8">
+        <Link href='/pages/hoa'>HOA</Link>
+        </div>
     </container>
   )
 }
